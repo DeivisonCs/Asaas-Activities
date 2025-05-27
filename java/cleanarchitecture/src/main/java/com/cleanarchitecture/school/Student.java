8bbfdaa080bafa09698a34f14353fb8a8b08d4b5
Deivison Cassimiro <deivison.cassimiro@asaas.com.br>
2025-05-27 10:39:39 -0300

    phone number unity tests

--- java/cleanarchitecture/src/main/java/com/cleanarchitecture/school/Student.java
+++ java/cleanarchitecture/src/main/java/com/cleanarchitecture/school/Student.java
@@ -1,7 +1,16 @@
 package com.cleanarchitecture.school;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private CPF cpf;
     private String name;
     private Email email;
+
+    private List<PhoneNumber> phoneNumbers = new ArrayList<>();
+
+    public void addPhoneNumber(String ddd, String number){
+        phoneNumbers.add(new PhoneNumber(ddd, number));
+    }
 }
---
+++ java/cleanarchitecture/src/test/java/school/PhoneNumberTest.java
@@ -0,0 +1,23 @@
+package school;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.cleanarchitecture.school.PhoneNumber;
+
+public class PhoneNumberTest {
+
+    @Test
+    void InvalidArguments() {
+        assertThrows(IllegalArgumentException.class, 
+            () -> new PhoneNumber(null, null));
+
+        assertThrows(IllegalArgumentException.class, 
+            () -> new PhoneNumber("", ""));
+
+        assertThrows(IllegalArgumentException.class, 
+            () -> new PhoneNumber("24", "91111"));
+            
+    }
+}
